c6b992c65fe6070a732c74c025c9d0ff882a0173
Tim Stibbs <tstibbs@users.noreply.github.com>
2021-01-28 19:27:20 +0000

    Improving validation of cf scripts

--- test/basic.mjs
+++ test/basic.mjs
@@ -1,22 +1,13 @@
 import {strictEqual} from 'assert'
-import {validate} from '@tstibbs/cloud-core-utils'
+import {validateWithExit} from '@tstibbs/cloud-core-utils'
 
 import dist from '../dist/main.js'
 
 //just check it imports, relatively little value in testing it properly
 import '../deploy/deploy-stack.mjs'
 
-async function run() {
-	try {
-		//check it exposes a function, that's all we can do without proper unit tests
-		strictEqual(typeof dist.handler, 'function')
+//check it exposes a function, that's all we can do without proper unit tests
+strictEqual(typeof dist.handler, 'function')
 
-		//validate cf script
-		await validate('deploy/template.yml')
-	} catch (e) {
-		console.error(e)
-		process.exit(1)
-	}
-}
-
-run()
+//validate cf script
+validateWithExit('deploy/template.yml')
